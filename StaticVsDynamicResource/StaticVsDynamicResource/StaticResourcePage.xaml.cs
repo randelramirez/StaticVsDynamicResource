57f18210bf1eb3a479cdbe4a4e62d1e39bc17b96
randelramirez <randelramirez1@gmail.com>
2016-10-19 20:29:50 +0800

    Add project files.

---
+++ StaticVsDynamicResource/StaticVsDynamicResource/StaticResourcePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace StaticVsDynamicResource
+{
+    public partial class StaticResourcePage : ContentPage
+    {
+        public StaticResourcePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
